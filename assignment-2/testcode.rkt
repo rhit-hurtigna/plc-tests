a582f9a3ff425b833d436f9f5820f1d94e3e5089
rhit-hurtigna <hurtigna@rose-hulman.edu>
2021-11-14 18:07:39 -0500

    Starting to get an import hierarchy going

---
+++ assignment-2/testcode.rkt
@@ -0,0 +1,21 @@
+#lang racket
+
+(require "../testcode-base.rkt")
+
+; To use these tests:
+;   fill out the stubs in assignment-2.rkt
+;   run this file in DrRacket
+;   (r) or (run-all)
+;   (run-test test-name) to run a portion of the tests
+
+; If you find errors, fix them, run this file again, and type (r) again.
+
+(define test (make-test
+  (add-me)
+  (sub-me)
+))
+
+(define run-all (make-run-all test))
+(define r run-all)
+(define individual-test (make-individual-test test))
+(define get-weights (make-get-weights test))
