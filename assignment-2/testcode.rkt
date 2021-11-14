69592f4e842048b3be346765c65070c61e4e9ff1
rhit-hurtigna <hurtigna@rose-hulman.edu>
2021-11-14 18:52:13 -0500

    Adding a testcase and a requirement for the file

--- assignment-2/testcode.rkt
+++ assignment-2/testcode.rkt
@@ -1,6 +1,7 @@
 #lang racket
 
 (require "../testcode-base.rkt")
+(require "assignment-2.rkt")
 
 ; To use these tests:
 ;   fill out the stubs in assignment-2.rkt
@@ -11,6 +12,7 @@
 ; If you find errors, fix them, run this file again, and type (r) again.
 
 (define test (make-test
-  (add-me)
+  (add-me
+   ((add-me 1 2) 3 1))
   (sub-me)
 ))
